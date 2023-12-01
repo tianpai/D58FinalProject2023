044a6ce5ee28f3621d86027b4155dcf5c0e62e78
tianpai <tianpai.zhang@mail.utoronto.ca>
2023-11-30 21:42:48 -0500

    compiled

--- src/encrypt.c
+++ src/encrypt.c
@@ -2,59 +2,48 @@
  * SIMPLE VPN CSCD58 final project
  */
 
-
-/* NOTE: 
+/* NOTE:
  *  1. ensure that error handling is comprehensive and robust
- *  2. This file assumes the necessary cerfiticate and key files are set up 
- *  3. SSL methods may have some compatibility issues 
+ *  2. This file assumes the necessary cerfiticate and key files are set up
+ *  3. SSL methods may have some compatibility issues
  *  4. Some more configuration may be required
  */
 
-
 #include "encrypt.h"
 
-/* The methods can be changed according to final 
+/* The methods can be changed according to final
  * compatibility requirements
  */
 #define SSL_CLIENT_METHOD SSLv23_client_method()
 #define SSL_SERVER_METHOD SSLv23_server_method()
 
-
 /* @brief: Initialize the SSL library
  * @param: None
  * @return: None
  */
-void init_ssl_lib()
-{
-  SSL_load_error_strings();         
-  OpenSSL_add_ssl_algorithms();     
+void init_ssl_lib() {
+  SSL_load_error_strings();
+  OpenSSL_add_ssl_algorithms();
 }
-
 
 /* @brief: Cleanup the SSL library
  * @param: None
  * @return: None
  */
-void cleanup_ssl_lib()
-{
-  EVP_cleanup();     
-}
-
+void cleanup_ssl_lib() { EVP_cleanup(); }
 
 /* @brief: Create a SSL context for the server
  * @param: None
  * @return: SSL_CTX * - SSL context
  */
-SSL_CTX *create_ssl_server_context()
-{
+SSL_CTX *create_ssl_server_context() {
   const SSL_METHOD *method;
   SSL_CTX *ctx;
 
-  method = SSL_SERVER_METHOD
+  method = SSL_SERVER_METHOD;
   ctx = SSL_CTX_new(method);
 
-  if (!ctx) 
-  {
+  if (!ctx) {
     perror("Unable to create SSL context");
     ERR_print_errors_fp(stderr);
     exit(EXIT_FAILURE);
@@ -62,22 +51,19 @@ SSL_CTX *create_ssl_server_context()
 
   return ctx;
 }
-
 
 /* @brief: Create a SSL context for the client
  * @param: None
  * @return: SSL_CTX * - SSL context
  */
-SSL_CTX *create_ssl_client_context()
-{
+SSL_CTX *create_ssl_client_context() {
   const SSL_METHOD *method;
   SSL_CTX *ctx;
 
-  method = SSL_CLIENT_METHOD
+  method = SSL_CLIENT_METHOD;
   ctx = SSL_CTX_new(method);
 
-  if (!ctx) 
-  {
+  if (!ctx) {
     perror("Unable to create SSL context");
     ERR_print_errors_fp(stderr);
     exit(EXIT_FAILURE);
@@ -85,7 +71,6 @@ SSL_CTX *create_ssl_client_context()
 
   return ctx;
 }
-
 
 /* @brief: Configure the SSL context
  * @param: SSL_CTX *ctx - SSL context
@@ -93,27 +78,22 @@ SSL_CTX *create_ssl_client_context()
  * @param: char *key_file - path to the key file
  * @return: None
  */
-void configure_context(SSL_CTX *ctx, char *cert_path, char *key_file)
-{
+void configure_context(SSL_CTX *ctx, char *cert_path, char *key_file) {
   /* cerfiticate file */
   int cfile = SSL_CTX_use_certificate_file(ctx, cert_path, SSL_FILETYPE_PEM);
   int privkey = SSL_CTX_use_PrivateKey_file(ctx, key_file, SSL_FILETYPE_PEM);
-  
-  if (cfile <= 0 || privkey <= 0) 
-  {
+
+  if (cfile <= 0 || privkey <= 0) {
     ERR_print_errors_fp(stderr);
     exit(EXIT_FAILURE);
   }
 
-  /* verify private key */ 
-  if (!SSL_CTX_check_private_key(ctx)) 
-  {
+  /* verify private key */
+  if (!SSL_CTX_check_private_key(ctx)) {
     fprintf(stderr, "Private key does not match the public certificate\n");
     exit(EXIT_FAILURE);
   }
 }
-
-
 
 /* @brief: Create a SSL connection
  * @param: int sockfd - socket file descriptor
@@ -122,12 +102,10 @@ void configure_context(SSL_CTX *ctx, char *cert_path, char *key_file)
  *          NULL - if error
  *          0    - if success
  */
-SSL *ssl_connect(int sockfd, SSL_CTX *ctx)
-{
-  SSL *ssl; 
+SSL *ssl_connect(int sockfd, SSL_CTX *ctx) {
+  SSL *ssl;
   SSL_set_fd(ssl, sockfd);
-  if (ssl_connect(ssl) <= 0) 
-  {
+  if (SSL_connect(ssl) <= 0) {
     ERR_print_errors_fp(stderr);
     SSL_free(ssl);
     return NULL;
@@ -135,30 +113,29 @@ SSL *ssl_connect(int sockfd, SSL_CTX *ctx)
   return ssl;
 }
 
-
 /* @brief: Accept a SSL connection
  * @param: SSL_CTX *ctx - SSL context
- * @return: SSL * - SSL connection 
+ * @return: SSL * - SSL connection
  *          0  - if success
  *          -1 - if error
  */
-int ssl_accept_connection(SSL_CTX *ssl)
-{
-  if (SSL_accept(ssl) <= 0) 
-  {
+int ssl_accept_connection(SSL_CTX *ssl) {
+  SSL *ssl_conn = SSL_new(ssl); /* create new SSL connection */
+
+  /* set socket file descriptor */
+  if (SSL_accept(ssl_conn) <= 0) {
     ERR_print_errors_fp(stderr);
+    SSL_free(ssl_conn);
     return -1;
   }
   return 0;
 }
 
-
 /* @brief: Shutdown a SSL connection
  * @param: SSL *ssl - SSL connection
  * @return: None
  */
-void ssl_shutdown(SSL *ssl)
-{
+void ssl_shutdown(SSL *ssl) {
   SSL_shutdown(ssl);
   SSL_free(ssl);
 }
--- src/encrypt.h
+++ src/encrypt.h
@@ -1,17 +1,22 @@
 /* Created by Tianpai Zhang Nov 13, 2023
-* For CSCD58 final project
-*/
+ * For CSCD58 final project
+ */
 
 #ifndef ENCRYPT_H
-#define ENCRYPT_H  
+#define ENCRYPT_H
 
-/* NEED TO INCLUDE THIS IN THE SHELL: 'sudo apt-get install libssl-dev' */
-#include <openssl/ssl.h>
+/* Ubuntu
+ * 'sudo apt-get install libssl-dev'
+ *
+ * Fedora
+ *  sudo dnf install openssl-devel
+ * */
 #include <openssl/err.h>
+#include <openssl/evp.h>
+#include <openssl/ssl.h>
 
-
-void init_ssl_lib();                /* initialize the SSL library */
-void cleanup_ssl_lib();             /* cleanup the SSL library */
+void init_ssl_lib();    /* initialize the SSL library */
+void cleanup_ssl_lib(); /* cleanup the SSL library */
 
 /* create a new SSL context */
 SSL_CTX *create_ssl_server_context();
@@ -23,7 +28,6 @@ void configure_context(SSL_CTX *ctx, char *cert_path, char *key_file);
 /* create a new SSL connection */
 SSL *ssl_connect(int sockfd, SSL_CTX *ctx);
 int ssl_accept_connection(SSL_CTX *ssl); /* handle individual connection */
-
 
 void ssl_shutdown(SSL *ssl);
 
