b182eccc321cb03dac8163d51aac6a85e56f3c02
Tianpai Z <tianpai.zhang@mail.utoronto.ca>
2023-12-05 16:17:35 -0500

    refactored

--- src/packet.c
+++ src/packet.c
@@ -11,6 +11,8 @@
 #include "protocol.h"
 #include "utils.h"
 
+#define VIR_IF "eth0"
+
 #define PACKET_SIZE(header)                                                    \
   (uint8_t)(payload_size + sizeof(header) + sizeof(gre_hdr_t) +                \
             sizeof(ip_hdr_t) + sizeof(ethernet_hdr_t));
@@ -53,45 +55,58 @@ void set_ip(ip_hdr_t *ip, char *ip_dest, char *ip_host, uint8_t ip_protocol,
   ip->ip_sum = cksum(ip, sizeof(ip_hdr_t));
 }
 
+inline ethernet_hdr_t *get_eth_hdr(uint8_t *packet_start) {
+  return (ethernet_hdr_t *)packet_start;
+}
+
+inline ip_hdr_t *get_ip_hdr(uint8_t *packet_start) {
+  return (ip_hdr_t *)(packet_start + sizeof(ethernet_hdr_t) +
+                      sizeof(gre_hdr_t));
+}
+
+inline tcp_hdr_t *get_tcp_hdr(uint8_t *packet_start) {
+  return (tcp_hdr_t *)(packet_start + sizeof(ethernet_hdr_t) +
+                       sizeof(gre_hdr_t) + sizeof(ip_hdr_t));
+}
+
 /* Not finished; Might be due for major (possibly delete later) */
 int create_packets(char *eth_dest, char *ip_dest, uint8_t ip_protocol,
-                   uint8_t payload, unsigned int payload_size) {
+                   uint8_t *payload, unsigned int payload_size, uint8_t flags) {
+  /*
+   * NOTE:
+   * Create a new packet and set it up
+   * The new packet should look like this:
+   *  --------------------------------------------------------
+   * |ethernet header|gre header|ip header|tcp header|payload |
+   * --------------------------------------------------------
+   */
 
-  /* Getting MAC and IP address for source */
-  char *eth_host =
-      get_mac_address("eth0"); /* Change name of interface depending on fit */
-  char *ip_host =
-      get_ip_address("eth0"); /* Change name of interface depending on fit */
+  /*Change name of interface depending on fit */
+  char *eth_host = get_mac_address(VIR_IF);
+  char *ip_host = get_ip_address(VIR_IF);
 
   uint8_t packet_size = get_packet_size(ip_protocol, payload_size);
 
-  /* Allocating memory for a new packet and setting it up */
-  uint8_t *new_packet = (uint8_t *)malloc(packet_size);
-  memset(new_packet, 0, sizeof(uint8_t) * (packet_size));
+  /* why not use calloc? calloc set the memory to 0 automatically */
+  /*
+   * uint8_t *new_packet = (uint8_t *)malloc(packet_size);
+   * memset(new_packet, 0, sizeof(uint8_t) * (packet_size));
+   */
+  uint8_t *new_packet = (uint8_t *)calloc(packet_size, sizeof(uint8_t));
 
-  /* Setting ethernet header info */
-  ethernet_hdr_t *new_eth = (ethernet_hdr_t *)new_packet;
+  ethernet_hdr_t *new_eth = get_eth_hdr(new_packet);
   set_eth(new_eth, eth_dest, eth_host);
-
-  /* Encapsulating the packet with GRE */
   packet_encapsulate(new_packet);
 
-  /* Setting IP header info. */
-  ip_hdr_t *new_ip =
-      (ip_hdr_t *)(new_packet + sizeof(ethernet_hdr_t) + sizeof(gre_hdr_t));
+  ip_hdr_t *new_ip = get_ip_hdr(new_packet);
   set_ip(new_ip, ip_dest, ip_host, ip_protocol, payload_size);
 
-  /* Checking if new packet is TCP */
-  if (ip_protocol == ip_protocol_tcp) {
-    tcp_hdr_t *new_tcp = (tcp_hdr_t *)(new_packet + sizeof(ethernet_hdr_t) +
-                                       sizeof(gre_hdr_t) + sizeof(ip_hdr_t));
-    new_tcp->src_port = PORT;
-    new_tcp->dst_port = 80; /* PORTS might need to be changed later */
-    /* Rest of TCP headers will need to be finished later */
-    /* The new packet is ICMP header */
-  } else {
-    icmp_hdr_t *new_icmp = (icmp_hdr_t *)(new_packet + sizeof(ethernet_hdr_t) +
-                                          sizeof(gre_hdr_t) + sizeof(ip_hdr_t));
-  }
+  /* ==== For now, just assume the new packet is TCP ==== */
+  tcp_hdr_t *new_tcp = get_tcp_hdr(new_packet);
+  new_tcp->src_port = PORT;
+  new_tcp->dst_port = 80; /* PORTS might need to be changed later */
+  new_tcp->flags = flags;
+  new_tcp->tcp_sum = cksum(new_tcp, sizeof(tcp_hdr_t));
+
   return -1; /* for now */
 }
