7c86b957cfd953aadb090c72835d65fe98954b30
Tianpai Z <tianpai.zhang@mail.utoronto.ca>
2023-12-08 01:51:56 -0800

    add parsing for str liter ip and mac addr

--- src/host_info.c
+++ src/host_info.c
@@ -1,4 +1,7 @@
 #include "host_info.h"
+#include "protocol.h"
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -7,6 +10,25 @@ HostInfo hosts[] = {
     {"h4", H4_IP, H4_MAC}, {"h5", H5_IP, H5_MAC}, {"h6", H6_IP, H6_MAC},
     {"h7", H7_IP, H7_MAC}, {NULL, NULL, NULL},
 };
+
+void parse_mac_addr(uint8_t eth[ETHER_ADDR_LEN], const char *mac_addr) {
+  for (int i = 0; i < ETHER_ADDR_LEN; i++) {
+    unsigned int byte;
+    sscanf(mac_addr, "%2x", &byte);
+    eth[i] = (uint8_t)byte;
+  }
+}
+
+void parse_ip_addr(uint32_t *ip, const char *ip_addr) {
+  unsigned int bytes[4];
+  if (sscanf(ip_addr, "%u.%u.%u.%u", &bytes[0], &bytes[1], &bytes[2],
+             &bytes[3]) == 4) {
+    *ip = (bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | bytes[3];
+  } else {
+    fprintf(stderr, "Error: when parsing IP address of hosts");
+    fprintf(stderr, "Invalid IP address: %s\n", ip_addr);
+  }
+}
 
 const char *get_host_ip(const char *host_name) {
   int i = 0;
