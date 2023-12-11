d3d9e6b19141ad2b837bb52b6c4b9a70227b62b5
kmstr87 <in.kim0128@gmail.com>
2023-12-10 21:27:10 -0500

    Fixed the ip changing issue and memory leak in server.c

--- src/server.c
+++ src/server.c
@@ -174,9 +174,16 @@ int main(int argc, char const *argv[]) {
   uint8_t *enc_pkt = (uint8_t *)calloc(packet_size, sizeof(uint8_t));
   packet_encapsulate(enc_pkt);
   memcpy((uint8_t *)(enc_pkt + sizeof(gre_hdr_t)), rec_packet_dest, sizeof(ip_hdr_t) + sizeof(tcp_hdr_t) + MAX_PAYLOAD_SIZE);
+
+  char *client_str_ip = malloc(4 * 4 * sizeof(char));
+  parse_ip_addr_to_str(client_str_ip, client_ip);
+
+  serv_handle_pkt(enc_pkt, client_str_ip);
   print_packet(enc_pkt);
   send_and_free_packet_vpn(new_socket, enc_pkt, ip_protocol_tcp, strlen((char *)(enc_pkt + sizeof(gre_hdr_t) + sizeof(ip_hdr_t) + sizeof(tcp_hdr_t))));
 
+  /* Freeing any allocs */
+  free(rec_packet_dest);
   /* closing the connected socket */
   close(new_socket);
   /* Shutting down the server */
